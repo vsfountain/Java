22f2da15ba33a611ad917a33de7c7291fa3092cb
phillipjpride <phillipjpride@gmail.com>
2018-10-29 14:15:58 -0400

    Updates to homework 1 and project 0

--- Phillip_Pride_Code/Homework1Java/src/com/homework/problemthirteen/BTriangle.java
+++ Phillip_Pride_Code/Homework1Java/src/com/homework/problemthirteen/BTriangle.java
@@ -4,14 +4,13 @@ public class BTriangle {
 
 	public static void main(String[] args) {
 		int num = 0;
-		// String[] strArry = { "0", "1 0", "1 0 1", "0 1 0 1" };
 		for (int i = 0; i < 4; i++) {
-			for (int j = 0; j < i+1; j++) {
+			for (int j = 0; j < i + 1; j++) {
 				System.out.print(num);
-				if(num==0) {
-					num=1;
-				}else {
-					num=0;
+				if (num == 0) {
+					num = 1;
+				} else {
+					num = 0;
 				}
 			}
 			System.out.println();
