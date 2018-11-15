a572172467d820e5099ffbf498c8b200809b6cbd
unknown <tempGitEmail@mail.com>
2018-11-15 00:02:19 -0500

    commitm

--- Michael_Jung_Code/homework2/Homework2.sql
+++ Michael_Jung_Code/homework2/Homework2.sql
@@ -189,13 +189,129 @@ END;
 
 
 --4.3
+select * from customer;
+
+CREATE OR REPLACE PROCEDURE get_name_and_company_of_customer(CUSTOMERID IN NUMBER, firstname OUT 
+                                            customer.firstname%TYPE, lastname OUT
+                                            customer.lastname%TYPE, company OUT
+                                            customer.company%TYPE)
+IS
+BEGIN
+    SELECT firstname INTO firstname, lastname INTO lastname, company INTO company from CUSTOMER where CUSTOMERID=CUSTOMERID
+END;
+/
+
+
+--5.0
+
+select * from invoice;
+
+select * from invoiceline;
+
+CREATE OR REPLACE PROCEDURE delete_invoice(INVOICEIDa IN INVOICE.invoiceid%type)
+IS
+BEGIN
+    DELETE FROM INVOICELINE where invoiceid=INVOICEIDa;
+    DELETE FROM INVOICE where invoiceid=INVOICEIDa;
+    COMMIT;
+END;
+/
 
 
 
+BEGIN
+    delete_invoice(111);
+END;
+/
+
+select * from customer;
+
+CREATE OR REPLACE PROCEDURE insert_new_customer_record
+    (firstname in customer.firstname%type,
+    lastname in customer.lastname%type,
+    company in customer.company%type,
+    address in customer.address%type,
+    city in customer.city%type,
+    state in customer.state%type,
+    country in customer.country%type,
+    postalcode in customer.postalcode%type,
+    phone in customer.phone%type,
+    fax in customer.fax%type,
+    email in customer.email%type,
+    supportrepid in customer.supportrepid%type)
+IS
+BEGIN
+    INSERT INTO CUSTOMER VALUES((select max(customerid) from customer) + 1,
+                            firstname,
+                            lastname,
+                            company,
+                            address,
+                            city,
+                            state,
+                            country,
+                            postalcode,
+                            phone,
+                            fax,
+                            email,
+                            supportrepid
+                            );
+                COMMIT;
+END;
+/
 
 
 
+--6.1
+
+CREATE OR REPLACE TRIGGER after_employee_insert
+AFTER INSERT ON employee
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('HELLO!');
+END;
+/
 
 
+CREATE OR REPLACE TRIGGER after_album_update
+AFTER UPDATE ON album
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('HELLO!');
+END;
+/
+
+CREATE OR REPLACE TRIGGER after_customer_delete
+AFTER DELETE ON customer
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('HELLO!');
+END;
+/
+
+
+
+--7.1
+select customer.firstname, customer.lastname, invoice.invoiceid
+from customer inner join invoice 
+on customer.customerid = invoice.customerid;
+
+--7.2
+select customer.customerid, customer.firstname,
+        customer.lastname, invoice.invoiceid,
+        invoice.total
+from customer left outer join invoice
+on (customer.customerid = invoice.customerid);
+
+--7.3
+select artist.name, album.title
+from artist right outer join album
+on artist.artistid=album.artistid;
+
+--7.4
+select artist.name
+from album cross join artist
+order by artist.name asc;
+
+--7.5
+select * from 
+employee e1, employee 
+where e1.reportsto=employee.reportsto;
 
 
