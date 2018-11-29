e8edcf07f038168240744ada98f27bc39c807026
TChester <Trevin.Chester@gmail.com>
2018-11-29 09:27:25 -0500

    hibernate stuffs

---
+++ 06-hibernate/HelloHibernate/src/main/java/com/example/util/HibernateUtil.java
@@ -0,0 +1,34 @@
+package com.example.util;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/*
+ * (class)
+ * Configuration's job is to gather information from hibernate.cfg.xml
+ *  and is used to create a session Factory.
+ *  
+ *  (interface)
+ *  SessionFactory's job is to create sessions and store information on
+ *   how to make connections to your database. Once it's configured it's
+ *   immutable.
+ *   
+ *  (interface)
+ *  Session manages the connection to your database and provides
+ *  	create, read, update, and delete operations.
+ *  
+ *  (interface)
+ *  Transaction manages...well....your transactions and cache (must be ACID)
+ */
+
+public class HibernateUtil {
+
+	private static SessionFactory sf =
+			new Configuration().configure("hibernate.cfg.xml")
+			.buildSessionFactory();
+	
+	public static Session getSession() {
+		return sf.openSession();
+	}
+}
