5d01e2c9823329f79a2adc09d5756c60ebcc008d
Kristen M Kavanagh-Wright <kkavana1@my.westga.edu>
2018-12-02 10:23:10 -0500

    trying

--- Hibernate project/kristenhibernate/kristenhibernate/src/main/java/com/kristen/Main.java
+++ Hibernate project/kristenhibernate/kristenhibernate/src/main/java/com/kristen/Main.java
@@ -1,17 +1,12 @@
 package com.kristen;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.hibernate.Session;
-
 import com.kristen.dao.CrimeDao;
 import com.kristen.dao.SuperVillianDao;
-import com.kristen.model.Crime;
+import com.kristen.model.SuperVillain;
 import com.kristen.util.HibernateUtil;
 
 public class Main {
-	public static final SuperVillianDao scdao = new SuperVillianDao();
+	public static final SuperVillianDao svdao = new SuperVillianDao();
 	public static final CrimeDao cdao = new CrimeDao();
 
 	public static void main(String[] args) {
@@ -24,30 +19,33 @@ public class Main {
 
 	}
 
-private static void insertInitialValues() {
-//		//CRimes
-//		Crime crime1 = new Crime("Aggravated Assault", "Causing Bodily Harm");
-//		Crime crime2 = new Crime("Arson", "Birning buildings, land or property");
-//		Crime crime3 = new Crime("Pineapples on Pizza", "who would do such a thing?");
-//		Crime crime4 = new Crime("The Twilight series", "Those movies are illegal, right?");
-//cdao.insert(crime1);
-//cdao.insert(crime2);
-//cdao.insert(crime3);
-//cdao.insert(crime4);
-//
-////Villains
-//List<Crime>CrimeList = new ArrayList<>();
-//crimeList.add(crime1);
-//crimeList.add(crime4);
-//	}
-	SuperVillain vill = new SuperVillain("Syndrome","power Remote", 50_000);
-	scdao.insert(vill);
-	vill = new SuperVillain("ScreenSlaver", "hypnotism",70_000);
-	svdao.insert(vill);
-	vill = new Supervillain ("Joker", "Infectious Laugh", 123_500);
-	svdao.insert(vill);
-	vill = new SuperVillain("Scar","Toomuch reading",99_000);
-	svdao.insert(vill);
-	vill = new SuperVillain("Yzma","Longeyelashes",50);
-	svdao.insert(vill);
+	private static void insertInitialValues() {
+		// //CRimes
+		// Crime crime1 = new Crime("Aggravated Assault", "Causing Bodily Harm");
+		// Crime crime2 = new Crime("Arson", "Birning buildings, land or property");
+		// Crime crime3 = new Crime("Pineapples on Pizza", "who would do such a
+		// thing?");
+		// Crime crime4 = new Crime("The Twilight series", "Those movies are illegal,
+		// right?");
+		// cdao.insert(crime1);
+		// cdao.insert(crime2);
+		// cdao.insert(crime3);
+		// cdao.insert(crime4);
+		//
+		//// Villains
+		// List<Crime>CrimeList = new ArrayList<>();
+		// crimeList.add(crime1);
+		// crimeList.add(crime4);
+		// }
+		SuperVillain vill = new SuperVillain("Syndrome", "power Remote", 50_000);
+		svdao.insert(vill);
+		vill = new SuperVillain("ScreenSlaver", "hypnotism", 70_000);
+		svdao.insert(vill);
+		vill = new SuperVillain("Joker", "Infectious Laugh", 123_500);
+		svdao.insert(vill);
+		vill = new SuperVillain("Scar", "Toomuch reading", 99_000);
+		svdao.insert(vill);
+		vill = new SuperVillain("Yzma", "Longeyelashes", 50);
+		svdao.insert(vill);
+	}
 }
--- Hibernate project/kristenhibernate/kristenhibernate/src/main/java/com/kristen/dao/SuperVillianDao.java
+++ Hibernate project/kristenhibernate/kristenhibernate/src/main/java/com/kristen/dao/SuperVillianDao.java
@@ -1,4 +1,14 @@
 package com.kristen.dao;
+
+import java.util.List;
+
+import org.hibernate.Transaction;
+
+import org.hibernate.Session;
+
+import com.kristen.model.SuperVillain;
+import com.kristen.util.HibernateUtil;
+
 /*
  * Session methods
  * save() and persist() result in a sql insert
@@ -13,12 +23,51 @@ package com.kristen.dao;
  * 
  */
 public class SuperVillianDao {
-public SuperVillainDao() {
+	public SuperVillianDao() {
+
+	}
+
+	public void insert(SuperVillain myVill) {
+		Session ses = HibernateUtil.getSession();
+		Transaction tx = ses.beginTransaction();
+		ses.save(myVill);
+		tx.commit();
+		// ses.close(); was demo that it was not a required,
+		// we also removed it to fix an exception the problem was that the main never
+		// had access to the session because it was being closed
+		// within each DAO method.
+	}
+	public void update (SuperVillain myVill) {
+		Session ses = HibernateUtil.getSession();
+		Transaction tx = ses.beginTransaction();
+		ses.update(myVill);
+		tx.commit();
+		//ses.close(); demo  for exception error
+	}
+	public SuperVillain selectByID (int id) {
+		Session ses = HibernateUtil.getSession();
+		SuperVillain myVill = ses.get(SuperVillain.class, id);
+		//ses.close();
+		return myVill;
+	}
+	public SuperVillain selectByName(String name) {
+		Session ses = HibernateUtil.getSession();
+		/*HQL
+		 * the attribute needs to be in single quotes
+		 * HQL uses Hibernate class names instead of the able names*/
+		
+		 List <SuperVillain> villList = ses.createQuery("from SuperVillain" +" whwre name ="+name, SuperVillain.class).list();
+		 SuperVillain myVill=villList.get(0);
+		 return myVill;
+	}
 	
-}
-public void insert (SuperVillain my Vill) {
-	Session ses =
-}
-
-
+	public List<SuperVillain>selectAll(){
+		Session ses = HibernateUtil.getSession();
+		List<SuperVillain> villList = ses.createQuery("from SuperVillain",SuperVillain.class).list();
+		//ses.close();
+		return villList;
+		
+		 
+	
+	}
 }
