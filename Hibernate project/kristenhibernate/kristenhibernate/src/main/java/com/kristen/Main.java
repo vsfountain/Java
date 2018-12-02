a5e3ee5455e9c81a3b582876cd8e81f37e40becf
Kristen M Kavanagh-Wright <kkavana1@my.westga.edu>
2018-12-02 09:46:07 -0500

    working

--- Hibernate project/kristenhibernate/kristenhibernate/src/main/java/com/kristen/Main.java
+++ Hibernate project/kristenhibernate/kristenhibernate/src/main/java/com/kristen/Main.java
@@ -16,7 +16,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// not required because of confirguration and session factory created is in the
-		// hibernate.cfg.xml file
+		// hibernate.cfg.xml file also in the dao
 		// Session ses = HibernateUtil.getSession();
 		// ses.close();
 		insertInitialValues();
@@ -24,20 +24,30 @@ public class Main {
 
 	}
 
-	private static void insertInitialValues() {
-		//CRimes
-		Crime crime1 = new Crime("Aggravated Assault", "Causing Bodily Harm");
-		Crime crime2 = new Crime("Arson", "Birning buildings, land or property");
-		Crime crime3 = new Crime("Pineapples on Pizza", "who would do such a thing?");
-		Crime crime4 = new Crime("The Twilight series", "Those movies are illegal, right?");
-cdao.insert(crime1);
-cdao.insert(crime2);
-cdao.insert(crime3);
-cdao.insert(crime4);
-
-//Villains
-List<Crime>CrimeList = new ArrayList<>();
-crimeList.add(crime1);
-crimeList.add(crime4);
-	}
+private static void insertInitialValues() {
+//		//CRimes
+//		Crime crime1 = new Crime("Aggravated Assault", "Causing Bodily Harm");
+//		Crime crime2 = new Crime("Arson", "Birning buildings, land or property");
+//		Crime crime3 = new Crime("Pineapples on Pizza", "who would do such a thing?");
+//		Crime crime4 = new Crime("The Twilight series", "Those movies are illegal, right?");
+//cdao.insert(crime1);
+//cdao.insert(crime2);
+//cdao.insert(crime3);
+//cdao.insert(crime4);
+//
+////Villains
+//List<Crime>CrimeList = new ArrayList<>();
+//crimeList.add(crime1);
+//crimeList.add(crime4);
+//	}
+	SuperVillain vill = new SuperVillain("Syndrome","power Remote", 50_000);
+	scdao.insert(vill);
+	vill = new SuperVillain("ScreenSlaver", "hypnotism",70_000);
+	svdao.insert(vill);
+	vill = new Supervillain ("Joker", "Infectious Laugh", 123_500);
+	svdao.insert(vill);
+	vill = new SuperVillain("Scar","Toomuch reading",99_000);
+	svdao.insert(vill);
+	vill = new SuperVillain("Yzma","Longeyelashes",50);
+	svdao.insert(vill);
 }
--- Hibernate project/kristenhibernate/kristenhibernate/src/main/java/com/kristen/dao/SuperVillianDao.java
+++ Hibernate project/kristenhibernate/kristenhibernate/src/main/java/com/kristen/dao/SuperVillianDao.java
@@ -1,5 +1,24 @@
 package com.kristen.dao;
-
+/*
+ * Session methods
+ * save() and persist() result in a sql insert
+ * update(0 and merge() result in a sql update
+ * delete() result in a sql delete
+ * saveOrUpdate() results in either a sql insert or update (depending)
+ * get() and load() results in a sql select
+ * get ()will go to the database immediately
+ * load() will use a placeholer(called a proxy) until you need the value
+ * update() will not allow duplicate ids insude of your cache
+ * merge will insert into the cache or overwrite the existing cache value
+ * 
+ */
 public class SuperVillianDao {
+public SuperVillainDao() {
+	
+}
+public void insert (SuperVillain my Vill) {
+	Session ses =
+}
+
 
 }
