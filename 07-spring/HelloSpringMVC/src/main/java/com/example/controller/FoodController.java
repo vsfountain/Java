a50ac8a57f2b50fec4e5191977a91f6347b3f308
TChester <Trevin.Chester@gmail.com>
2018-12-05 15:51:32 -0500

    issues with spring, early push

--- 07-spring/HelloSpringMVC/src/main/java/com/example/controller/FoodController.java
+++ 07-spring/HelloSpringMVC/src/main/java/com/example/controller/FoodController.java
@@ -3,9 +3,11 @@ package com.example.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.model.Food;
@@ -21,9 +23,17 @@ public class FoodController {
 	public FoodController() {
 	}
 	
-	@GetMapping(value="/getAllFood.app")
+	//@GetMapping(value="/getAllFood.app")
+	@RequestMapping(value="/getAllFood.app", method=RequestMethod.GET)
 	public @ResponseBody List<Food> getAllFoods() {
 		return foodDao.selectAll();
+	}
+	
+	//http://localhost:9005/HelloSpringMVC/enriko/getFoodById.app?id=2
+	@RequestMapping(value="/getFoodById.app", method=RequestMethod.POST,
+			produces="application/json", params= {"id"})
+	public ResponseEntity<Food> getFoodById(int id) {
+		return new ResponseEntity<Food>(foodDao.selectById(id), HttpStatus.OK);
 	}
 	
 	
