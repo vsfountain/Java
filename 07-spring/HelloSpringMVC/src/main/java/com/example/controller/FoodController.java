fda2b2f89012eef7593941b5b3455e8cb1854954
TChester <Trevin.Chester@gmail.com>
2018-12-05 16:35:31 -0500

    spring mvc

--- 07-spring/HelloSpringMVC/src/main/java/com/example/controller/FoodController.java
+++ 07-spring/HelloSpringMVC/src/main/java/com/example/controller/FoodController.java
@@ -3,11 +3,12 @@ package com.example.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.model.Food;
@@ -15,6 +16,7 @@ import com.example.repository.FoodDao;
 
 @Controller
 @RequestMapping(value="/enriko")
+//@CrossOrigin(stuff)
 public class FoodController {
 
 	@Autowired
@@ -30,11 +32,21 @@ public class FoodController {
 	}
 	
 	//http://localhost:9005/HelloSpringMVC/enriko/getFoodById.app?id=2
-	@RequestMapping(value="/getFoodById.app", method=RequestMethod.POST,
-			produces="application/json", params= {"id"})
-	public ResponseEntity<Food> getFoodById(int id) {
-		return new ResponseEntity<Food>(foodDao.selectById(id), HttpStatus.OK);
+	/*@RequestMapping(value="/getFoodById.app", method=RequestMethod.POST,
+			produces="application/json", params= {"num"})
+	public ResponseEntity<Food> getFoodById(int num) {
+		return new ResponseEntity<Food>(foodDao.selectById(num), HttpStatus.OK);
+	}*/
+	
+	@PostMapping(value="/getFoodById.app")
+	public @ResponseBody Food getFoodById(@RequestParam int num) {
+		return foodDao.selectById(num);
 	}
 	
+	//http://localhost:9005/HelloSpringMVC/enriko/4/getFoodByUri.app
+	@PostMapping(value="{num}/getFoodByUri.app")
+	public @ResponseBody Food getFoodByUri(@PathVariable("num") int num) {
+		return foodDao.selectById(num);
+	}
 	
 }
