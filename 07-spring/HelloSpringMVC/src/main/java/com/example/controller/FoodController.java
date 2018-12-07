03191bd963809bbfab5d87f36e501f8b243b15dc
Clement Dikoko <obialet.dikoko@outlook.com>
2018-12-07 09:07:27 -0500

    untracked

--- 07-spring/HelloSpringMVC/src/main/java/com/example/controller/FoodController.java
+++ 07-spring/HelloSpringMVC/src/main/java/com/example/controller/FoodController.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.model.Food;
@@ -21,10 +24,26 @@ public class FoodController {
 	public FoodController() {
 	}
 	
-	@GetMapping(value="/getAllFood.app")
+//	@GetMapping(value="/getAllFood.app")
+	@RequestMapping(value="/getAllFood.app", method=RequestMethod.GET)
 	public @ResponseBody List<Food> getAllFoods() {
 		return foodDao.selectAll();
 	}
 	
+	/*@RequestMapping(value="/getFood.app", method=RequestMethod.POST, produces="application/json", params= {"id"})
+	public ResponseEntity<Food> getFoodById(int id){
+		return new ResponseEntity<Food>(foodDao.selectById(id), HttpStatus.OK);
+		
+	}
+*/	
+	//same as before different way
+	@PostMapping(value="/getFoodById.app")
+	public @ResponseBody Food getFoodById(@RequestParam int num){
+		return foodDao.selectById(num);
+	}
 	
+	@PostMapping(value="{num}/getFoodUri.app")
+	public @ResponseBody Food getFoodByURI(@PathVariable("num") int num) {
+		return foodDao.selectById(num);
+	}
 }
---
+++ 07-spring/HelloSpringMVC/src/main/java/com/example/controller/ViewController.java
@@ -0,0 +1,13 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ViewController {
+	@RequestMapping(value="/next.app", method=RequestMethod.GET)
+	public String getPage() {
+		return "nextpage.html";
+	}
+}
